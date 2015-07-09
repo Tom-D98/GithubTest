61cab841618fe1545108f6c125e1be10c8909f24
Tom-D98 <tom.davies98@outlook.com>
2015-07-09 16:48:10 +0100

    Update Index.php
    Updated CS

--- Index.php
+++ Index.php
@@ -1,8 +1,8 @@
 <p> Hello World</p>
 
-	<?php
-		$list = array("1","2","3");
-		foreach($list as $item){
-	 		echo "<p> {$item} </p>";
-	 	}
-	 ?>	
+<?php
+	$list = array("1","2","3");
+	foreach($list as $item){
+	 	echo "<p> {$item} </p>";
+ 	}
+ ?>
